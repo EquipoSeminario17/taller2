ad6accffb7fd6f68dc2678c96743a3d7a1fa47bf
byron Mt <baironmt@gmail.com>
2022-09-24 19:24:20 -0500

    avance 2

--- js/taller.js
+++ js/taller.js
@@ -1,172 +1,181 @@
-class calcular{
-    constructor(){
-        this.empl = new Empleado;
-    }
-    darEmpleado(){
-        return this.empl
-    }
-    nuSalario(sal){
-        this.empl.caSalario(sal);
-    }
-    calcularPrestaciones(prestacion1, prestacion2){
-        this.empl.caPrestaciones
-    }
+class Calculos{
+	constructor(){
+		this.emple = new Empleado;
+	}
+
+	darEmpleado(){
+		return this.emple;
+	}
+
+	nuSalario(sal){
+		this.emple.casalario(sal);
+	}
+
+	calcularEdad(anoN,mesN,diaN,anoA,mesA,diaA){
+		this.emple.caedad(anoN,mesN,diaN,anoA,mesA,diaA);
+	}
+
+	calcularAntiguedad(anoI,mesI,diaI,anoA,mesA,diaA){
+		this.emple.cantiguedad(anoI,mesI,diaI,anoA,mesA,diaA);
+	}
+
+	calcularPrestaciones(presS,presA){
+		this.emple.caprestaciones(presS,presA);
+	}
+
+
+
 }
 
 class Empleado{
-    constructor(nombre, apellido, sexo ){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.sexo = sexo ;
-        this. ingreso = 0;
-        this. sala=0;
-        this.calculoPrestaciones=0;
+	constructor(nomre,apellido,sexo){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.sexo = sexo;
+		this.nacimiento = 0;
+		this.ingreso = 0;
+		this.sala = 0;
+		this.calculoEdad = 0;
+		this.calculoAntiguedad = 0;
+		this.calculoPrestaciones = 0;
+	
 
-    }
-    darNombre(){
-        return this.nombre;
-    }
-     darApellido(){
-        return this.apellido;
-     }
-     darSexo(){
-        return this.sexo;
-     }
-     darIngreso(){
-        return this.ingreso
-     }
-     darSalario(){
-        return this.salario;
-     }
-     darCalculoPrestaciones(){
-        return this.calculoPrestaciones;
-     }
-     caSalario(sal){
-        this.salario = sal;
-     }
 
-     caPrestaciones(prestacion1, prestacion2){
-        this.calculoPrestaciones = (prestacion1*prestacion2)/12;
+	}
 
-     }
+	darNombre(){
+		return this.nombre;
+	}
 
+	darApellido(){
+		return this.apellido;
+	}
+
+	darSexo(){
+		return this.sexo;
+	}
+
+	darNacimiento(){
+		return this.nacimiento;
+	}
+
+	darIngreso(){
+		return this.ingreso;
+	}
+
+	darSalario(){
+		return this.salario;
+	}
+
+	darCalculoEdad(){
+		return this.calculoEdad;
+	}
+
+	darCalculoAntiguedad(){
+		return this.calculoAntiguedad;
+	}
+
+	darCalculoPrestaciones(){
+		return this.calculoPrestaciones;
+	}
+
+	
+
+	casalario(sal){
+		this.salario = sal;
+	}
+
+	caedad(anoN,mesN,diaN,anoA,mesA,diaA){
+		let eda = anoA - anoN;
+		if (mesA > mesN) {
+			eda--;
+	        this.calculoEdad = eda+1;
+	    } else if (mesA < mesN) {
+	        	eda--;
+	            this.calculoEdad = eda;
+	        }else if(mesA == mesN){
+	        		if (diaA < diaN) {
+	        			eda--;
+	            		this.calculoEdad = eda;
+	    			} else{
+	    				eda--;
+	            		this.calculoEdad = eda+1;
+	    			}
+	    	}
+	}
+
+	cantiguedad(anoI,mesI,diaI,anoA,mesA,diaA){
+		let anti = anoA - anoI;
+		if (mesA > mesI) {
+			anti--;
+	        this.calculoAntiguedad = anti+1;
+	    } else if (mesA < mesI) {
+	        	anti--;
+	            this.calculoAntiguedad = anti;
+	        }else if(mesA == mesI){
+	        		if (diaA < diaI) {
+	        			anti--;
+	            		this.calculoAntiguedad = anti;
+	    			} else{
+	    				anti--;
+	            		this.calculoAntiguedad = anti+1;
+	    			}
+	    	}
+	}
+
+	caprestaciones(presS,presA){
+		this.calculoPrestaciones = (presS*presA)/12;
+	}
+
+	
 }
- let calc = new Calculos();
 
- function salario_btn(){
- let sala = document.getElementById('salario1').value;
- calc.nuSalario(parseInt(sala));
- const s= calc.darEmpleado().darSalario();
- alert('nuevo Salario $' + sala );
- }
+let cal = new Calculos();
 
- function prestaciones_btn(){
-    let psal = document.getElementById("salario1").value;
-    let pant = document.getElementById("calcantiguedad").value;
-    calc.calcularPrestaciones(psal, pant);
-    const p = calc.darEmpleado().darCalculoPrestaciones();
-    document.getElementById("calcprestaciones").value=p;
- }
-
-
-function f_fecha() {
-    var fecha = document.getElementById("fecha").value;
-    var fechaNac = new Date(fecha);
-    var mesF = fechaNac.getMonth() + 1;
-    var anioF = fechaNac.getFullYear();
-    var diaF = fechaNac.getDate();
-
-
-
-
-    var fechaAct = new Date();
-    var mesH = fechaAct.getMonth() +1;
-    var anioH = fechaAct.getFullYear();
-    var diaH = fechaAct.getDate();
-
-    var diasSum = diasFin(anioF,mesF);
-
-     var mesAdicional =0;
-     if ( diaF > diaH){
-        diaH = parseInt(diaH) + parseInt(diasSum);
-        mesAdicional =1;
-     }
-     var dias =diaH - diaF;
-
-     var anioAdicional = 0;
-     if (mesF > mesH)
-     {
-        mesH = parseInt(mesH) +12;
-        anioAdicional = 1;
-     }
-
-     var meses = parseInt(mesH) -(parseInt(mesF) + parseInt(mesAdicional));
-
-     var anios = parseInt(anioH) - (parseInt(anioF) + parseInt(anioAdicional));
-
-     
-document.getElementById('calcularedad').value=anios+" años";
-
-
+function salario_btn(){
+	let sala = document.getElementById('salario').value;
+	cal.nuSalario(parseInt(sala));
+	const s = cal.darEmpleado().darSalario();
+	alert("Nuevo salario $"+sala);
 }
-function diasFin(anio, mes)
-{
-    var anionAux =anio;
-    if(anio < 12)
-    {
-        var mesSig =parseInt(mes) +1;
-    
 
+function edad_btn(){
+	let fechaNacimiento = document.getElementById('nacimiento').value;
+    const anoNacimiento = parseInt(String(fechaNacimiento).substring(0, 4));
+    const mesNacimiento = parseInt(String(fechaNacimiento).substring(5, 7));
+    const diaNacimiento = parseInt(String(fechaNacimiento).substring(8, 10));
+
+    const fechaActual = new Date();
+    const anoActual = parseInt(fechaActual.getFullYear());
+    const mesActual = parseInt(fechaActual.getMonth()) + 1;
+    const diaActual = parseInt(fechaActual.getDate());
+
+    cal.calcularEdad(anoNacimiento, mesNacimiento, diaNacimiento, anoActual, mesActual, diaActual);
+    let anio = cal.darEmpleado().darCalculoEdad();
+    document.getElementById('caledad').value = anio ;
 }
-else {
-    var mesSig =1;
-    anionAux = anio + 1;
+
+function antiguedad_btn(){
+	let fechaIngreso = document.getElementById("ingreso").value;
+    const anoIngreso = parseInt(String(fechaIngreso).substring(0, 4));
+    const mesIngreso = parseInt(String(fechaIngreso).substring(5, 7));
+    const diaIngreso = parseInt(String(fechaIngreso).substring(8, 10));
+
+    const fechaActual = new Date();
+    const anoActual = parseInt(fechaActual.getFullYear());
+    const mesActual = parseInt(fechaActual.getMonth()) + 1;
+    const diaActual = parseInt(fechaActual.getDate());
+
+    cal.calcularAntiguedad(anoIngreso, mesIngreso, diaIngreso, anoActual, mesActual, diaActual);
+    let a = cal.darEmpleado().darCalculoAntiguedad();
+    document.getElementById('calantig').value = a;
 }
- var fecha= anionAux+"-"+mesSig+"+01";
- ms =Date.parse(fecha);
- fechaD = new Date(ms);
- fechaD.setDate(fechaD.getDate() -1);
- var diaH = fechaD.getDate();
 
- return diaH;
+function prestaciones_btn(){
+	let psal = document.getElementById("salario").value;
+	let pant = document.getElementById("calantig").value;
+	cal.calcularPrestaciones(psal, pant);
+	const p = cal.darEmpleado().darCalculoPrestaciones();
+	document.getElementById('calpresta').value = p;
 }
-function btn_ingreso(){
 
-    var fecha = document.getElementById("ingreso").value;
-    var fechaNac = new Date(fecha);
-    var mesF = fechaNac.getMonth() + 1;
-    var anioF = fechaNac.getFullYear();
-    var diaF = fechaNac.getDate();
-
-
-
-
-    var fechaAct = new Date();
-    var mesH = fechaAct.getMonth() +1;
-    var anioH = fechaAct.getFullYear();
-    var diaH = fechaAct.getDate();
-
-    var diasSum = diasFin(anioF,mesF);
-
-     var mesAdicional =0;
-     if ( diaF > diaH){
-        diaH = parseInt(diaH) + parseInt(diasSum);
-        mesAdicional =1;
-     }
-     var dias =diaH - diaF;
-
-     var anioAdicional = 0;
-     if (mesF > mesH)
-     {
-        mesH = parseInt(mesH) +12;
-        anioAdicional = 1;
-     }
-
-     var meses = parseInt(mesH) -(parseInt(mesF) + parseInt(mesAdicional));
-
-     var anios = parseInt(anioH) - (parseInt(anioF) + parseInt(anioAdicional));
-
-     
-document.getElementById('calcantiguedad').value=anios+" años";
-
-}
